3f0d38f7e04065cdf15e845d7e182a270ea70458
lumin <ashwathybasil@gmail.com>
2024-05-13 14:16:24 +0530

    first commit

---
+++ src/basics/Hellopgm.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class Hellopgm {
+
+	public static void main(String[] args) {
+	byte b=127;
+	System.out.println("value of b is:"+b);
+	
+	int a=12345;
+	System.out.println("value of a is:" +a);
+	
+	long l=1233665587l;
+	System.out.println("value of l is"+l);
+	
+	double d=3.556;
+	System.out.println("value of d is:"+d);
+	float e=3.88f;
+	System.out.println("value of f is"+e);
+	char c='d';
+	System.out.println(c);
+	String s="Anu";
+	System.out.println(s);
+	boolean y=50>40;
+	System.out.println(y);
+	
+	
+	
+	}
+
+}
